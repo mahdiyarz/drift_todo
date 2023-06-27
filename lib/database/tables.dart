a4e2fe090368ae2bd29e201259c97b1bbb9066f7
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2023-06-27 20:37:27 +0330

    feat: create tag table

--- lib/database/tables.dart
+++ lib/database/tables.dart
@@ -4,7 +4,15 @@ import 'package:drift/drift.dart';
 class TasksTbl extends Table {
   //* autoIncrement automatically sets this to be the primary key
   IntColumn get id => integer().autoIncrement()();
+  IntColumn get tagId => integer().nullable().references(TagsTbl, #id)();
   TextColumn get title => text().withLength(min: 1, max: 50)();
   DateTimeColumn get dueDate => dateTime().nullable()();
   BoolColumn get isCompleted => boolean().withDefault(const Constant(false))();
+}
+
+@DataClassName('TagEntity')
+class TagsTbl extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 1, max: 50)();
+  IntColumn get color => integer()();
 }
